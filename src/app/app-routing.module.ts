50f322c9c863304fb425e1ba6d29d388b100943c
balassit <blakelass@gmail.com>
2018-09-03 10:07:23 -0700

    about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
   { path: '**', component: NotFoundComponent}
 ];
 
